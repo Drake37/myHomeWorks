8f8866bc6390a0af716e13e73c36f50e1aa1c0f1
Roman <6830698+Drake37@users.noreply.github.com>
2022-02-10 17:28:31 +0300

    hw 4, task 1-2 beta

---
+++ arrays_lib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_lib
+{
+    public static class StaticClass
+    {
+        public static int GetPairCount(int[] ar)
+        {
+            int count = 0;
+            for(int i = 0; i < ar.Length -1; i++)
+            {
+                if (ar[i] % 3 == 0 ^ ar[i + 1] % 3 == 0) count++;
+            }
+            return count;
+        }
+    }
+}
